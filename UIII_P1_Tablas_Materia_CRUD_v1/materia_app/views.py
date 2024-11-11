0820e051d9ab62771d592a8c583cab1cfced6bfe
SebassRM128 <a.22308051281303@cbtis128.edu.mx>
2024-11-11 10:23:05 -0700

    Add files via upload

---
+++ UIII_P1_Tablas_Materia_CRUD_v1/materia_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect
+from .models import Materia
+# Create your views here.
+def inicio_vista(request):
+    lasmaterias=Materia.objects.all()
+    return render(request,'gestionarMaterias.html',{"mismaterias":lasmaterias})
+
+def registrarMateria(request):
+    codigo=request.POST["txtcodigo"]
+    nombre=request.POST["txtnombre"]
+    creditos=request.POST["numcreditos"]
+
+    guardarmateria=Materia.objects.create(
+        codigo=codigo,nombre=nombre,creditos=creditos
+        # GUARDA EL REGISTRO
+    )
+    return redirect("/")
